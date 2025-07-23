351d6b3b4f5f5626f81d10dab066afb4ba15fe27
Jayson <macedajayson11@gmail.com>
2025-07-23 13:33:52 +0800

    update - index,app.razor and program.cs (Client)

--- GenstarXKulayInventorySystem.Client/Program.cs
+++ GenstarXKulayInventorySystem.Client/Program.cs
@@ -1,11 +1,20 @@
 using GenstarXKulayInventorySystem.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.TopRight;
+    config.SnackbarConfiguration.HideTransitionDuration = 100;
+    config.SnackbarConfiguration.ShowTransitionDuration = 100;
+    config.SnackbarConfiguration.VisibleStateDuration = 3000;
+    config.SnackbarConfiguration.ShowCloseIcon = true;
+});
 await builder.Build().RunAsync();
