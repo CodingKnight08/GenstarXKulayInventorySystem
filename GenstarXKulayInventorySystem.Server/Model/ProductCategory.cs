2dfb7748c1c763e811918cdd540ebfa795a42e18
Jayson <macedajayson11@gmail.com>
2025-07-24 21:19:52 +0800

    update - product category

--- GenstarXKulayInventorySystem.Server/Model/ProductCategory.cs
+++ GenstarXKulayInventorySystem.Server/Model/ProductCategory.cs
@@ -8,5 +8,5 @@ public class ProductCategory
    [Required]
    public string Name { get; set; } = string.Empty;
    public string? Description { get; set; } = string.Empty;
-   public ICollection<Product> Products { get; set; } = new List<Product>();
+   public ICollection<Product>? Products { get; set; } = new List<Product>();
 }
