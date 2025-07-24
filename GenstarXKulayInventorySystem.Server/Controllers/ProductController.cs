03b5b2a0d8af4327201deffa2ffc4d67c7fd431e
Jayson <macedajayson11@gmail.com>
2025-07-24 21:24:38 +0800

    create - ProductController

---
+++ GenstarXKulayInventorySystem.Server/Controllers/ProductController.cs
@@ -0,0 +1,71 @@
+﻿using GenstarXKulayInventorySystem.Server.Services;
+using GenstarXKulayInventorySystem.Shared.DTOS;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GenstarXKulayInventorySystem.Server.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class ProductController : ControllerBase
+{
+    private readonly IProductService _productService;
+
+    public ProductController(IProductService productService)
+    {
+        _productService = productService;
+    }
+
+    // GET: api/products
+    [HttpGet]
+    public async Task<ActionResult<List<ProductDto>>> GetAll()
+    {
+        var products = await _productService.GetAllAsync();
+        return Ok(products);
+    }
+
+    // GET: api/products/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<ProductDto?>> GetById(int id)
+    {
+        var product = await _productService.GetByIdAsync(id);
+        if (product == null)
+            return NotFound();
+
+        return Ok(product);
+    }
+
+    // POST: api/products
+    [HttpPost]
+    public async Task<ActionResult> Create(ProductDto dto)
+    {
+        var result = await _productService.AddAsync(dto);
+        if (!result)
+            return BadRequest("Product already exists.");
+        return Ok();
+    }
+
+    // PUT: api/products/5
+    [HttpPut("{id}")]
+    public async Task<ActionResult> Update(int id, ProductDto dto)
+    {
+        if (id != dto.Id)
+            return BadRequest();
+
+        var result = await _productService.UpdateAsync(dto);
+        if (!result)
+            return NotFound();
+
+        return NoContent();
+    }
+
+    // DELETE: api/products/5
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> Delete(int id)
+    {
+        var result = await _productService.DeleteAsync(id);
+        if (!result)
+            return NotFound();
+
+        return NoContent();
+    }
+}
