e26e8dd046ec5a725df4121d1c589f7379af9832
Jayson <macedajayson11@gmail.com>
2025-07-24 21:20:04 +0800

    create - helpers

---
+++ GenstarXKulayInventorySystem.Shared/Helpers/ProductsEnumHelpers.cs
@@ -0,0 +1,26 @@
+﻿namespace GenstarXKulayInventorySystem.Shared.Helpers;
+
+public class ProductsEnumHelpers
+{
+    public enum BranchOption
+    {
+        Polomolok,
+        GeneralSantosCity,
+        Warehouse,
+    }
+
+    public enum  ProductMesurementOption
+    {
+        Piece,
+        Kilogram,
+        Liter,
+        Box,
+        Yard,
+        Meter,
+        Gram,
+        Centimeter,
+        Gallon,
+
+    }
+
+}
