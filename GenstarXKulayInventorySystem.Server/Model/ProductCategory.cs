7aa300015b7ec7e8f361c6dd848e1fb524e50a37
Jayson <macedajayson11@gmail.com>
2025-07-26 08:40:46 +0800

    update - Product Category model and add migrations

--- GenstarXKulayInventorySystem.Server/Model/ProductCategory.cs
+++ GenstarXKulayInventorySystem.Server/Model/ProductCategory.cs
@@ -2,7 +2,7 @@
 
 namespace GenstarXKulayInventorySystem.Server.Model;
 
-public class ProductCategory
+public class ProductCategory:BaseEntity
 {
    public int Id { get; set; }
    [Required]
--- GenstarXKulayInventorySystem.Shared/DTOS/ProductCategoryDto.cs
+++ GenstarXKulayInventorySystem.Shared/DTOS/ProductCategoryDto.cs
@@ -1,6 +1,6 @@
 ﻿namespace GenstarXKulayInventorySystem.Shared.DTOS;
 
-public class ProductCategoryDto
+public class ProductCategoryDto:BaseEntityDto
 {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
