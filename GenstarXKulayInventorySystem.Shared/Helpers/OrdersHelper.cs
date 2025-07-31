b62bc15906390a57cfbc3ee0c962760c4ddacbf3
Jayson <macedajayson11@gmail.com>
2025-07-31 10:19:06 +0800

    create - ordersHelper

---
+++ GenstarXKulayInventorySystem.Shared/Helpers/OrdersHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenstarXKulayInventorySystem.Shared.Helpers;
+
+public class OrdersHelper
+{
+    public enum PurchaseRecieptOption
+    {
+        NonBIR,
+        BIR
+    }
+
+    public enum PurchaseRecieveOption
+    {
+       RecieveAll,
+       PartialRecieve,
+       Pending,
+    }
+
+    public enum  PurchaseShipToOption
+    {
+        GeneralSantosCity,
+        Polomolok,
+        Warehouse,
+    }
+}
