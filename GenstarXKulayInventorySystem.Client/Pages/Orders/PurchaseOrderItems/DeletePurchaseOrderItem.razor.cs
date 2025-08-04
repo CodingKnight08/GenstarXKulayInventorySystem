13ef9357b3f974c5195b2fdf5bd507a5f0ded7bb
Jayson <macedajayson11@gmail.com>
2025-08-04 17:31:34 +0800

    create - PurchaseOrderItems components

---
+++ GenstarXKulayInventorySystem.Client/Pages/Orders/PurchaseOrderItems/DeletePurchaseOrderItem.razor.cs
@@ -0,0 +1,43 @@
+﻿using GenstarXKulayInventorySystem.Shared.DTOS;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace GenstarXKulayInventorySystem.Client.Pages.Orders.PurchaseOrderItems;
+
+public partial class DeletePurchaseOrderItem
+{
+    [Parameter] public PurchaseOrderItemDto PurchaseOrderItem { get; set; } = default!;
+    [Inject] protected ILogger<DeletePurchaseOrderItem> Logger { get; set; } = default!;
+    [Inject] protected HttpClient HttpClient { get; set; } = default!;
+    [Inject] protected ISnackbar Snackbar { get; set; } = default!;
+    [CascadingParameter] protected IMudDialogInstance MudDialog { get; set; } = default!;
+
+    protected async Task DeletePurchaseOrderItemAsync()
+    {
+        try
+        {
+            var response = await HttpClient.DeleteAsync($"api/purchaseorderitem/{PurchaseOrderItem.Id}");
+            if (response.IsSuccessStatusCode)
+            {
+
+                Snackbar.Add("Purchase order item deleted successfully.", Severity.Success);
+                MudDialog.Close(DialogResult.Ok(PurchaseOrderItem));
+                Logger.LogInformation("Purchase order item deleted successfully.");
+            }
+            else
+            {
+                Logger.LogError("Failed to delete purchase order item. Status code: {StatusCode}", response.StatusCode);
+            }
+        }
+        catch (Exception ex)
+        {
+            Logger.LogError(ex, "An error occurred while deleting the purchase order item.");
+        }
+    }
+
+    protected void Cancel()
+    {
+        MudDialog.Cancel();
+        Logger.LogInformation("Delete operation cancelled.");
+    }
+}
---
+++ GenstarXKulayInventorySystem.Client/Pages/Orders/PurchaseOrderItems/GetAllToBeAddedPurchaseOrderItems.razor.cs
@@ -0,0 +1,80 @@
+﻿using GenstarXKulayInventorySystem.Shared.DTOS;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using System.Diagnostics.CodeAnalysis;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using static MudBlazor.CategoryTypes;
+
+namespace GenstarXKulayInventorySystem.Client.Pages.Orders.PurchaseOrderItems;
+
+public partial class GetAllToBeAddedPurchaseOrderItems
+{
+    [Parameter] public EventCallback<List<PurchaseOrderItemDto>> OnPurchaseOrderItemsChanged { get; set; }
+    [Inject] protected HttpClient HttpClient { get; set; } = default!;
+    [Inject] protected ISnackbar Snackbar { get; set; } = default!;
+    [Inject] protected IDialogService DialogService { get; set; } = default!;
+    [Inject] protected ILogger<GetAllToBeAddedPurchaseOrderItems> Logger { get; set; } = default!;
+
+    protected List<PurchaseOrderItemDto> PurchaseOrderItems { get; set; } = new();
+
+
+    protected override Task OnInitializedAsync()
+    {
+        return base.OnInitializedAsync();
+    }
+
+    protected async Task NotifyParent()
+    {
+        await OnPurchaseOrderItemsChanged.InvokeAsync(PurchaseOrderItems);
+    }
+    protected async Task AddPurchaseOrderItem()
+    {
+        try
+        {
+            var dialog = await DialogService.ShowAsync<CreatePurchaseOrderItem>("");
+            if (dialog is not null)
+            {
+                var result = await dialog.Result;
+                if (result is not null && !result.Canceled)
+                {
+                    var purchaseOrderItem = result.Data as PurchaseOrderItemDto;
+                    if (purchaseOrderItem is not null)
+                    {
+                        bool isDuplicate = PurchaseOrderItems.Any(p =>
+                            p.ProductBrandId == purchaseOrderItem.ProductBrandId &&
+                            p.ProductId == purchaseOrderItem.ProductId);
+
+                        if (!isDuplicate)
+                        {
+                            PurchaseOrderItems.Add(purchaseOrderItem);
+                            await NotifyParent();
+                            Snackbar.Add("Purchase Order Item added successfully.", Severity.Success);
+                            StateHasChanged();
+                        }
+                        else
+                        {
+                            Snackbar.Add("Purchase Order Item already exists.", Severity.Warning);
+                        }
+                    }
+
+                }
+            }
+        }
+        catch(Exception ex)
+        {
+            Logger.LogError($"Adding PO item dialog error, {ex}", Severity.Error);
+        }
+    }
+
+    protected async Task RemovePurchaseItem(int brandId, int productId)
+    {
+        var itemToRemove =PurchaseOrderItems.FirstOrDefault(item => item.ProductBrandId == brandId && item.ProductId == productId);
+        if (itemToRemove != null)
+        {
+            PurchaseOrderItems.Remove(itemToRemove);
+            await NotifyParent();
+        }
+        StateHasChanged();
+    }
+}
