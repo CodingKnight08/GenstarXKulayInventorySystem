f4ab1ae829356616ae022d3455c6d64c35f4cbe8
Jayson <macedajayson11@gmail.com>
2025-08-14 09:17:39 +0800

    create - DailySaleModel and migrations

---
+++ GenstarXKulayInventorySystem.Server/Migrations/20250813035541_CreateDailySaleModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GenstarXKulayInventorySystem.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateDailySaleModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DailySales",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DateOfSales = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    NameOfClient = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SalesNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RecieptReference = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Branch = table.Column<int>(type: "int", nullable: false),
+                    PaymentType = table.Column<int>(type: "int", nullable: false),
+                    SalesOption = table.Column<int>(type: "int", nullable: false),
+                    TotalAmount = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DailySales", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DailySales");
+        }
+    }
+}
