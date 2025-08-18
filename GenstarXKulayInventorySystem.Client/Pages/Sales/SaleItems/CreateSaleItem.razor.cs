8cc9b9a77edb3d5effa06db9ae47fd621354ca10
Jayson <macedajayson11@gmail.com>
2025-08-18 19:52:13 +0800

    create - SaleItems components

---
+++ GenstarXKulayInventorySystem.Client/Pages/Sales/SaleItems/CreateSaleItem.razor.cs
@@ -0,0 +1,192 @@
+﻿using GenstarXKulayInventorySystem.Shared.DTOS;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using static GenstarXKulayInventorySystem.Shared.Helpers.ProductsEnumHelpers;
+using static MudBlazor.Icons.Custom;
+
+namespace GenstarXKulayInventorySystem.Client.Pages.Sales.SaleItems;
+
+public partial class CreateSaleItem
+{
+    [Parameter] public BranchOption Branch { get; set; }
+    [Inject] protected HttpClient HttpClient { get; set; } = default!;
+    [Inject] protected ILogger<CreateSaleItem> Logger { get; set; } = default!;
+    [CascadingParameter] protected IMudDialogInstance MudDialog { get; set; } = default!;
+    [Inject] protected ISnackbar Snackbar { get; set; } = default!;
+    protected SaleItemDto SaleItemDto { get; set; } = new SaleItemDto();
+    protected List<ProductBrandDto> ProductBrands { get; set; } = new List<ProductBrandDto>();
+    protected List<ProductDto> Products { get; set; } = new List<ProductDto>();
+    protected ProductDto? SelectedProductFromList { get; set; } = new ProductDto();
+
+    protected ProductBrandDto? SelectedBrand { get; set; } = new ProductBrandDto();
+    protected string SelectedProduct { get; set; } = string.Empty;
+    protected bool IsLoading { get; set; } = false;
+    protected bool IsProductLoading { get; set; } = false;
+    protected bool IsWholeSale { get; set; } = false;
+    protected override async Task OnInitializedAsync()
+    {
+        await LoadBrands();
+    }
+
+    private async Task LoadBrands()
+    {
+        IsLoading = true;
+
+        try
+        {
+            var response = await HttpClient.GetAsync("api/productbrand/all");
+            response.EnsureSuccessStatusCode();
+            var brands = await response.Content.ReadFromJsonAsync<List<ProductBrandDto>>();
+            ProductBrands = brands ?? new List<ProductBrandDto>();
+        }
+        catch (Exception ex) {
+            Logger.LogError($"Error in loading brands: {ex.Message}");
+        }
+        finally
+        {
+            IsLoading = false;
+        }
+    }
+
+    private async Task LoadBrandProducts()
+    {
+        IsProductLoading = true;
+        try
+        {
+            var response = await HttpClient.GetAsync($"api/product/all/by/{SelectedBrand?.Id}/{Branch}");
+            response.EnsureSuccessStatusCode();
+            var products = await response.Content.ReadFromJsonAsync<List<ProductDto>>();
+            Products = products ?? new List<ProductDto>();
+
+        }
+        catch (Exception ex) { 
+            Logger.LogError($"{ex.Message}");
+        }
+        finally
+        {
+            IsProductLoading = false;
+        }
+    }
+    protected Task<IEnumerable<string>> SearchBrands(string value,CancellationToken cancellationToken)
+    {
+        if (ProductBrands is null || !ProductBrands.Any())
+            return Task.FromResult(Enumerable.Empty<string>());
+
+        var result = ProductBrands
+           .Where(b => !string.IsNullOrWhiteSpace(b.BrandName) && (string.IsNullOrWhiteSpace(value) || b.BrandName.Contains(value, StringComparison.OrdinalIgnoreCase)))
+           .Select(b => b.BrandName);
+
+        return Task.FromResult(result);
+    }
+
+    protected Task<IEnumerable<string>> SearchProducts(string value, CancellationToken cancellationToken)
+    { if (Products is null || !Products.Any()) 
+            return Task.FromResult(Enumerable.Empty<string>()); 
+        var result = Products
+            .Where(p => !string.IsNullOrWhiteSpace(p.ProductName) 
+            && (string.IsNullOrWhiteSpace(value) || p.ProductName
+            .Contains(value, StringComparison.OrdinalIgnoreCase))).Select(p => p.ProductName);
+        return Task.FromResult(result); }
+
+    protected void OnProductSelect(string product)
+    {
+        // Called when user picks from the list
+        var matchedProduct = Products.FirstOrDefault(e =>
+            !string.IsNullOrWhiteSpace(e.ProductName) &&
+            string.Equals(e.ProductName, product, StringComparison.OrdinalIgnoreCase));
+
+        if (matchedProduct != null)
+        {
+            SaleItemDto.ProductId = matchedProduct.Id;
+            SaleItemDto.ItemName = matchedProduct.ProductName;
+        }
+    }
+
+    protected void OnProductTyped(string text)
+    {
+        
+        SelectedProduct = text;
+        SaleItemDto.ItemName = text;
+
+        // Reset ProductId if no match
+        var matchedProduct = Products.FirstOrDefault(e =>
+            !string.IsNullOrWhiteSpace(e.ProductName) &&
+            string.Equals(e.ProductName, text, StringComparison.OrdinalIgnoreCase));
+
+        SaleItemDto.ProductId = matchedProduct?.Id;  // null if custom
+        SelectedProductFromList = matchedProduct ?? new ProductDto();
+        if(SelectedProductFromList is not null)
+        {
+            OnWholeSaleChanged(IsWholeSale);
+        }
+    }
+
+
+    protected async Task OnBrandSelect(string brand) {
+        if (string.IsNullOrWhiteSpace(brand) && ProductBrands == null)
+            return;
+
+        SelectedBrand = ProductBrands.FirstOrDefault(b =>
+            !string.IsNullOrWhiteSpace(b.BrandName) &&
+            string.Equals(b.BrandName, brand, StringComparison.OrdinalIgnoreCase)
+        );
+        if(SelectedBrand != null)
+        {
+            await LoadBrandProducts();
+            SelectedProduct = string.Empty;
+            SaleItemDto.ProductId = null;
+            SaleItemDto.ItemName = string.Empty;
+            StateHasChanged();
+        }
+    }
+
+    protected void SaveItem()
+    {
+        if(SaleItemDto.ProductId != null && IsWholeSale)
+        {
+            SaleItemDto.ProductPricingOption = ProductPricingOption.WholeSale;
+        }
+        else if (SaleItemDto.ProductId !=null && !IsWholeSale)
+        {
+            SaleItemDto.ProductPricingOption = ProductPricingOption.Retail;
+        }
+        else
+        {
+            SaleItemDto.ProductPricingOption = ProductPricingOption.Override;
+        }
+        Snackbar.Add("Item has been added!", Severity.Success);
+        MudDialog.Close(DialogResult.Ok(SaleItemDto));
+        
+    }
+    protected void Cancel()
+    {
+        MudDialog.Cancel();
+    }
+
+    protected bool ShouldShowSaleType()
+    {
+        return SaleItemDto.PaintCategory == PaintCategory.Solid
+               && SaleItemDto.ProductId != null;
+    }
+    protected void OnWholeSaleChanged(bool value)
+    {
+        IsWholeSale = value;
+
+        if (SelectedProductFromList == null)
+            return;
+
+        if (IsWholeSale)
+        {
+            SaleItemDto.ItemPrice = SelectedProductFromList.WholesalePrice.GetValueOrDefault();  // adjust property name
+        }
+        else
+        {
+            SaleItemDto.ItemPrice = SelectedProductFromList.RetailPrice.GetValueOrDefault();    // adjust property name
+        }
+
+        StateHasChanged(); // force UI update
+    }
+
+}
---
+++ GenstarXKulayInventorySystem.Client/Pages/Sales/SaleItems/GetAllSaleItemsToBeAdded.razor.cs
@@ -0,0 +1,51 @@
+﻿using GenstarXKulayInventorySystem.Shared.DTOS;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using static GenstarXKulayInventorySystem.Shared.Helpers.ProductsEnumHelpers;
+
+namespace GenstarXKulayInventorySystem.Client.Pages.Sales.SaleItems;
+
+public partial class GetAllSaleItemsToBeAdded
+{
+    [Parameter] public BranchOption Branch { get; set; }
+    [Parameter] public EventCallback<List<SaleItemDto>> OnSaleItemsChanged { get; set; }
+    [Inject] protected IDialogService Dialog { get; set; } = default!;
+    
+    protected List<SaleItemDto> SaleItemsToBeAdded { get; set; } = new List<SaleItemDto>();
+    protected bool IsLoading { get; set; } = false;
+
+    protected async Task AddSaleItem()
+    {
+        var dialog = await Dialog.ShowAsync<CreateSaleItem>("Add Sale Item",
+            new DialogParameters
+            {
+                { "Branch", Branch}
+            },
+            new DialogOptions
+            {
+                MaxWidth = MaxWidth.Medium,
+                FullWidth = true,
+                CloseButton = true,
+                BackdropClick = false
+            }
+        );
+
+        if(dialog != null)
+        {
+            var result = await dialog.Result;
+            if (result is not null && !result.Canceled) {
+                var saleItem = result.Data as SaleItemDto;
+                if (saleItem != null)
+                {
+                    // ✅ Add logic here, e.g.
+                    SaleItemsToBeAdded.Add(saleItem);
+                    StateHasChanged();
+                    await OnSaleItemsChanged.InvokeAsync(SaleItemsToBeAdded);
+                }
+            }
+
+        }
+    }
+
+    
+}
