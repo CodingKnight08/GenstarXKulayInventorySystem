c40a33921a2bcb7f17fa32492ae9c4424be44994
Jayson <macedajayson11@gmail.com>
2025-08-27 14:09:46 +0800

    update - SalesController

--- GenstarXKulayInventorySystem.Server/Controllers/SalesController.cs
+++ GenstarXKulayInventorySystem.Server/Controllers/SalesController.cs
@@ -45,6 +45,45 @@ public class SalesController : ControllerBase
         return Ok(sales);
     }
 
+    [HttpGet("all/paid/{date}")]
+    public async Task<ActionResult<List<DailySaleDto>>> GetAllPaidSalesByDate(DateTime date)
+    {
+        var sales = await _saleService.GetAllDailySalesPaidAsync(date);
+        if (sales == null || sales.Count == 0)
+            return NotFound($"No paid sales found for {date:yyyy-MM-dd}.");
+        return Ok(sales);
+    }
+
+    [HttpGet("all/unpaid/{date}")]
+    public async Task<ActionResult<List<DailySaleDto>>> GetAllUnpaidSalesByDate(DateTime date)
+    {
+        var sales = await _saleService.GetAllDailySalesUnpaidAsync(date);
+        if (sales == null || sales.Count == 0)
+            return NotFound($"No unpaid sales found for {date:yyyy-MM-dd}.");
+        return Ok(sales);
+    }
+
+
+    [HttpGet("all/invoices/{date}")]
+    public async Task<ActionResult<string>> GetAllSalesWithInvoicesByDate(DateTime date)
+    {
+        var totalInvoiceSale = await _saleService.GetAllDailyInvoiceAsync(date);
+
+        if (totalInvoiceSale <= 0)
+            return NotFound($"No sales with invoices found for {date:yyyy-MM-dd}.");
+
+        return Ok(totalInvoiceSale.ToString("F2") );
+    }
+
+    [HttpGet("all/nonvoices/{date}")]
+    public async Task<ActionResult<string>> GetAllSalesNonvoiceByDate(DateTime date)
+    {
+        var totalNonInvoiceSale = await _saleService.GetAllDailyNonVoiceAsync(date);
+        if (totalNonInvoiceSale <= 0)
+            return NotFound($"No sales without invoices found for {date:yyyy-MM-dd}.");
+        return Ok(totalNonInvoiceSale.ToString("F2"));
+    }
+
     [HttpGet("{id}")]
     public async Task<ActionResult<DailySaleDto?>> GetDailySaleById(int id)
     {
