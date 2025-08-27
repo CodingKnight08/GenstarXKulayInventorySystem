48069fa51e00a108c87950fb74b9761ff6253171
Jayson <macedajayson11@gmail.com>
2025-08-27 14:08:43 +0800

    create - DailySaleReportModel

---
+++ GenstarXKulayInventorySystem.Server/Migrations/20250827043404_UpdateDbContext.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GenstarXKulayInventorySystem.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDbContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DailySaleReports",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CashIn = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    TotalSalesToday = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    BeginningBalance = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    InvoiceCash = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    InvoiceChecks = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    NonInvoiceCash = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    NonInvoiceChecks = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    TotalCash = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    TotalChecks = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    TotalSales = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    ChargeSales = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    CollectionCash = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    CollectionChecks = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    Transportation = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    Foods = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    SalaryAndAdvances = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    Commissions = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    Supplies = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    Others = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    TotalExpenses = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    TotalCashOnHand = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    Notes = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PreparedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DailySaleReports", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DailySaleReports");
+        }
+    }
+}
---
+++ GenstarXKulayInventorySystem.Shared/DTOS/DailySaleReportDto.cs
@@ -0,0 +1,30 @@
+﻿namespace GenstarXKulayInventorySystem.Shared.DTOS;
+
+public class DailySaleReportDto: BaseEntityDto
+{
+    public int Id { get; set; }
+    public DateTime Date { get; set; }
+    public decimal CashIn { get; set; }
+    public decimal TotalSalesToday { get; set; }
+    public decimal BeginningBalance { get; set; }
+    public decimal InvoiceCash { get; set; }
+    public decimal InvoiceChecks { get; set; }
+    public decimal NonInvoiceCash { get; set; }
+    public decimal NonInvoiceChecks { get; set; }
+    public decimal TotalCash { get; set; }
+    public decimal TotalChecks { get; set; }
+    public decimal TotalSales { get; set; }
+    public decimal ChargeSales { get; set; }
+    public decimal CollectionCash { get; set; }
+    public decimal CollectionChecks { get; set; }
+    public decimal Transportation { get; set; }
+    public decimal Foods { get; set; }
+    public decimal SalaryAndAdvances { get; set; }
+    public decimal Commissions { get; set; }
+    public decimal Supplies { get; set; }
+    public decimal Others { get; set; }
+    public decimal TotalExpenses { get; set; }
+    public decimal TotalCashOnHand { get; set; }
+    public string Notes { get; set; } = string.Empty;
+    public string PreparedBy { get; set; } = string.Empty;
+}
