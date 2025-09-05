f790df9e75f2d894599406e3e2cc0620bbd2f2da
Jayson <macedajayson11@gmail.com>
2025-09-05 11:14:21 +0800

    create - UserState

---
+++ GenstarXKulayInventorySystem.Client/UserState.cs
@@ -0,0 +1,13 @@
+﻿using static GenstarXKulayInventorySystem.Shared.Helpers.ProductsEnumHelpers;
+
+namespace GenstarXKulayInventorySystem.Client;
+
+public class UserState
+{
+    public BranchOption? Branch { get; private set; }
+
+    public void SetBranch(BranchOption? branch)
+    {
+        Branch = branch;
+    }
+}
