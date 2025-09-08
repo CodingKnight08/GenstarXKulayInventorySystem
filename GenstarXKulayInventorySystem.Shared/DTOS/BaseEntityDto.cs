ef7a1e90ca1769759ec0d975969374fff902c024
Jayson <macedajayson11@gmail.com>
2025-09-08 23:02:50 +0800

    update- BaseEntityDto

--- GenstarXKulayInventorySystem.Shared/DTOS/BaseEntityDto.cs
+++ GenstarXKulayInventorySystem.Shared/DTOS/BaseEntityDto.cs
@@ -4,7 +4,7 @@ namespace GenstarXKulayInventorySystem.Shared.DTOS;
 
 public class BaseEntityDto
 {
-    public DateTime CreatedAt { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime? UpdatedAt { get; set; }
     public DateTime? DeletedAt { get; set; }
     public bool IsDeleted { get; set; } = false;
