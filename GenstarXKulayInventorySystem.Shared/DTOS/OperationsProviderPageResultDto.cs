b1dac5a7fed0bfefc30725d6a7a15f8eadb04ea7
Jayson <macedajayson11@gmail.com>
2025-09-30 22:31:26 +0800

    create - OperationsProvider Dto and PageResultDto

---
+++ GenstarXKulayInventorySystem.Shared/DTOS/OperationsProviderPageResultDto.cs
@@ -0,0 +1,7 @@
+﻿namespace GenstarXKulayInventorySystem.Shared.DTOS;
+
+public class OperationsProviderPageResultDto<OperationsProviderDto>
+{
+    public int TotalCount { get; set; }
+    public List<OperationsProviderDto> OperationsProviders { get; set; } = new List<OperationsProviderDto>();
+}
