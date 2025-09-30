8af6bf4e6376a54ac4ddaac1f3eb18e531dbedac
Jayson <macedajayson11@gmail.com>
2025-09-30 13:08:20 +0800

    create - OperationsProvider model and add field in Billing

---
+++ GenstarXKulayInventorySystem.Server/Model/OperationsProvider.cs
@@ -0,0 +1,10 @@
+﻿namespace GenstarXKulayInventorySystem.Server.Model;
+
+public class OperationsProvider:BaseEntity
+{
+    public int Id {get;set; }
+    public string ProviderName{ get; set; } = string.Empty;
+    public string TINNumber { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public ICollection<Billing> Billings { get; set; } = new List<Billing>();
+}
