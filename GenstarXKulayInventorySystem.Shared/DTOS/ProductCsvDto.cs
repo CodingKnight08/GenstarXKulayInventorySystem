b06cd10c9c78da7848016b99e3177d55c869ab6d
Jayson <macedajayson11@gmail.com>
2025-10-13 08:39:28 +0800

    create - ProductCsvDto

---
+++ GenstarXKulayInventorySystem.Shared/DTOS/ProductCsvDto.cs
@@ -0,0 +1,17 @@
+﻿namespace GenstarXKulayInventorySystem.Shared.DTOS;
+
+public class ProductCsvDto
+{
+    public string ProductMeasurementOption { get; set; } = string.Empty;
+    public decimal Size { get; set; }
+    public decimal SUPPLIERS_PRICE { get; set; }
+    public decimal FREIGHT { get; set; }
+    public decimal CostPrice { get; set; }
+    public decimal RetailPrice { get; set; }
+    public decimal WholesalePrice { get; set; }
+    public int ProductBrandId { get; set; }
+    public decimal ActualQuantity { get; set; }
+    public decimal BufferStocks { get; set; }
+    public int BranchId { get; set; }
+    public int Quantity { get; set; }
+}
