a44c51afc5149f49c619bb819565044228ba78a8
Jayson <macedajayson11@gmail.com>
2025-10-18 00:37:14 +0800

    update- DailySaleReportModel

--- GenstarXKulayInventorySystem.Server/Model/DailySaleReport.cs
+++ GenstarXKulayInventorySystem.Server/Model/DailySaleReport.cs
@@ -31,6 +31,9 @@ public class DailySaleReport:BaseEntity
     public string PreparedBy { get; set; } = string.Empty;
     public BranchOption Branch { get; set; }
 
+    public decimal LandedCost { get; set; }
+    public decimal GrossProfit { get; set; }
+
     public ICollection<Billing> Billings { get; set; } = new List<Billing>();
     public ICollection<DailySale> DailySales { get; set; } = new List<DailySale>();
 }
