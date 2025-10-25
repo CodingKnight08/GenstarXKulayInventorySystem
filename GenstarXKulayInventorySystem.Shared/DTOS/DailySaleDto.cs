5d903ca963fd70f63a33c2e0a1b6f47c24e10caf
CodingKnight08 <106590670+CodingKnight08@users.noreply.github.com>
2025-10-25 08:55:34 +0800

    Enhancement/dailysale/approved/101825 (#42)
    * update - ViewDailySale
    
    * update - CreateDailySale
    
    * update - EditDailySale
    
    * update -EditSaleItems
    
    * update- DailySaleDto

--- GenstarXKulayInventorySystem.Shared/DTOS/DailySaleDto.cs
+++ GenstarXKulayInventorySystem.Shared/DTOS/DailySaleDto.cs
@@ -22,6 +22,7 @@ public class DailySaleDto:BaseEntityDto
     public PurchaseRecieptOption? SalesOption { get; set; } 
     public decimal? TotalAmount { get; set; }
     public decimal? Commission { get; set; }
+    public bool IsPaid { get; set; }
     public bool HasDiscount { get; set; }
     public bool IsApproved { get; set; }
     public List<SaleItemDto> SaleItems { get; set; } = new List<SaleItemDto>();
