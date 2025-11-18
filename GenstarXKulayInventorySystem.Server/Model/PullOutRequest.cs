29f64c8367970c17e8b4df7377f36708d2a1ce8c
Jayson <macedajayson11@gmail.com>
2025-11-18 19:31:35 +0800

    create - models

---
+++ GenstarXKulayInventorySystem.Server/Model/PullOutRequest.cs
@@ -0,0 +1,15 @@
+﻿using static GenstarXKulayInventorySystem.Shared.Helpers.ProductsEnumHelpers;
+
+namespace GenstarXKulayInventorySystem.Server.Model;
+
+public class PullOutRequest:BaseEntity
+{
+    public int Id { get; set; }
+    public DateTime? DateRequest { get; set; }
+    public BranchOption BranchRequestee { get; set; }
+    public BranchOption BranchRequestedTo { get; set; }
+    public bool Delivered { get; set; }
+    public DateTime? DateDelivered { get; set; } = DateTime.UtcNow;
+    public string Note { get; set; } = string.Empty;
+    public ICollection<RequestProductItem> RequestProductItems { get; set; } = new HashSet<RequestProductItem>();
+}
