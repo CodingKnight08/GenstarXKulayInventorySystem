93413f0e7ba9749a0140b17fe16aacd9a8571c6a
Jayson <macedajayson11@gmail.com>
2025-11-19 15:55:48 +0800

    create - PullOut and RequestProductItem dto

---
+++ GenstarXKulayInventorySystem.Shared/DTOS/PullOutRequestDto.cs
@@ -0,0 +1,14 @@
+﻿using static GenstarXKulayInventorySystem.Shared.Helpers.ProductsEnumHelpers;
+
+namespace GenstarXKulayInventorySystem.Shared.DTOS;
+public class PullOutRequestDto:BaseEntityDto
+{
+    public int Id { get; set; }
+    public DateTime? DateRequest { get; set; }
+    public BranchOption BranchRequestee { get; set; }
+    public BranchOption BranchRequestedTo { get; set; }
+    public bool Delivered { get; set; }
+    public DateTime? DateDelivered { get; set; } = DateTime.UtcNow;
+    public string Note { get; set; } = string.Empty;
+    public List<RequestProductItemDto> RequestProductItems { get; set; } = new List<RequestProductItemDto>();
+}
---
+++ GenstarXKulayInventorySystem.Shared/DTOS/RequestProductItemDto.cs
@@ -0,0 +1,26 @@
+﻿using static GenstarXKulayInventorySystem.Shared.Helpers.ProductsEnumHelpers;
+
+namespace GenstarXKulayInventorySystem.Shared.DTOS;
+public class RequestProductItemDto:BaseEntityDto
+{
+    public int Id { get; set; }
+    public int? ProductId { get; set; } //product Id sa requester
+    public ProductDto? Product { get; set; }
+    public string ProductName { get; set; } = string.Empty;
+    public int RequestedQuantity { get; set; }
+    public int ReleasedQuantity { get; set; }
+    public bool IsReceived { get; set; }
+    public BranchOption Branch { get; set; }
+    public BranchOption SourceProduct { get; set; } = BranchOption.Warehouse;
+    public DateTime DateRecieved { get; set; }
+    public ProductRequestedDto ProductCode { get; set; } =  new ProductRequestedDto();
+    public string Remarks { get; set; } = string.Empty;
+}
+
+public class ProductRequestedDto
+{
+    public int ProductId { get; set; }
+    public string ProductName { get; set; } = string.Empty;
+    public int AvailableQuantity { get; set; } 
+
+    }
