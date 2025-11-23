18c6ea61923d04c1869deb0aa3bd2c0ec9e7f06d
Jayson <macedajayson11@gmail.com>
2025-11-23 09:30:26 +0800

    create - PullOutRequestComponents

---
+++ GenstarXKulayInventorySystem.Client/Pages/PullOutDelivery/CreatePullOutRequest.razor.cs
@@ -0,0 +1,16 @@
+﻿using GenstarXKulayInventorySystem.Shared.DTOS;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace GenstarXKulayInventorySystem.Client.Pages.PullOutDelivery;
+
+public partial class CreatePullOutRequest
+{
+    [CascadingParameter] protected IMudDialogInstance MudDialog { get; set; } = default!;
+    [Inject] protected HttpClient HttpClient { get; set; } = default!;
+    [Inject] protected ISnackbar SnackBar { get; set; } = default!;
+    [Inject] protected UserState UserState { get; set; } = default!;
+    [Inject] protected IDialogService DialogService { get; set; } = default!;
+    protected PullOutRequestDto NewPullOut { get; set; } = new PullOutRequestDto();
+
+}
---
+++ GenstarXKulayInventorySystem.Client/Pages/PullOutDelivery/GetAllRequestProducts.razor.cs
@@ -0,0 +1,68 @@
+﻿using GenstarXKulayInventorySystem.Shared.DTOS;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using System.Net.Http.Json;
+using static GenstarXKulayInventorySystem.Shared.Helpers.ProductsEnumHelpers;
+
+namespace GenstarXKulayInventorySystem.Client.Pages.PullOutDelivery;
+
+public partial class GetAllRequestProducts
+{
+    [Inject] public UserState UserState { get; set; } = default!;
+    [Inject] protected HttpClient HttpClient { get; set; } = default!;
+    [Inject] protected ISnackbar SnackBar { get; set; } = default!;
+    [Inject] protected IDialogService DialogService { get; set; } = default!;
+    [Inject] protected ILogger<GetAllRequestProducts> Logger { get; set; } = default!;
+    protected bool IsLoading { get; set; } = false;
+    protected List<PullOutRequestDto> PullOuts { get; set; } = new List<PullOutRequestDto>();
+    protected BranchOption Branch { get; set; }
+    protected override async Task OnInitializedAsync()
+    {
+        Branch = UserState.Branch ?? BranchOption.GeneralSantosCity;
+        await LoadData();
+    }
+
+    protected async Task LoadData()
+    {
+        IsLoading = true;
+        try
+        {
+            var response = await HttpClient.GetAsync($"api/pullout/all/requests/{Branch}");
+            if (response.IsSuccessStatusCode)
+            {
+                PullOuts = await response.Content.ReadFromJsonAsync<List<PullOutRequestDto>>() ?? new List<PullOutRequestDto>();
+                if(PullOuts.Count == 0)
+                {
+                    SnackBar.Add("No Pull Outs", Severity.Warning);
+                }
+                
+            }
+            else
+            {
+                SnackBar.Add("Failed to load pullouts", Severity.Error);
+            }
+        }
+        catch(Exception ex)
+        {
+            Logger.LogError(ex, "Error loading pullouts");
+            SnackBar.Add("Failed to load pull outs", Severity.Error);
+        }
+        finally
+        {
+            IsLoading = false;
+        }
+    }
+
+    protected async Task RequestPullOut()
+    {
+        try
+        {
+
+        }
+        catch(Exception ex)
+        {
+            Logger.LogError(ex.Message, "Error in creating pull out request");
+            SnackBar.Add("An error occured.", Severity.Error);
+        }
+    }
+}
