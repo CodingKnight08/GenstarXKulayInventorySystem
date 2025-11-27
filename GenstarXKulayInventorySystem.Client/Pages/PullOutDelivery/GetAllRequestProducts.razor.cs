376b2f6c51a25540c1920ecd2774b388460f7912
Jayson <macedajayson11@gmail.com>
2025-11-27 11:06:07 +0800

    update - GetAllRequestProducts

--- GenstarXKulayInventorySystem.Client/Pages/PullOutDelivery/GetAllRequestProducts.razor.cs
+++ GenstarXKulayInventorySystem.Client/Pages/PullOutDelivery/GetAllRequestProducts.razor.cs
@@ -1,4 +1,5 @@
-﻿using GenstarXKulayInventorySystem.Shared.DTOS;
+﻿using DocumentFormat.OpenXml.Spreadsheet;
+using GenstarXKulayInventorySystem.Shared.DTOS;
 using Microsoft.AspNetCore.Components;
 using MudBlazor;
 using System.Net.Http.Json;
@@ -13,6 +14,7 @@ public partial class GetAllRequestProducts
     [Inject] protected ISnackbar SnackBar { get; set; } = default!;
     [Inject] protected IDialogService DialogService { get; set; } = default!;
     [Inject] protected ILogger<GetAllRequestProducts> Logger { get; set; } = default!;
+    [Inject] protected NavigationManager NavigationManager { get; set; } = default!;
     protected bool IsLoading { get; set; } = false;
     protected List<PullOutRequestDto> PullOuts { get; set; } = new List<PullOutRequestDto>();
     protected BranchOption Branch { get; set; }
@@ -57,12 +59,40 @@ public partial class GetAllRequestProducts
     {
         try
         {
+            var options = new DialogOptions
+            {
+                CloseButton = true,
+                MaxWidth = MaxWidth.Large,
+                FullWidth = true,
+                BackdropClick = false
+            };
 
+            var dialog = await DialogService.ShowAsync<CreatePullOutRequest>(
+                "Create Pull-Out Request",
+                options
+            );
+
+            var result = await dialog.Result;
+
+            if (!result.Canceled)
+            {
+                // You can get data from dialog: result.Data
+                SnackBar.Add("Pull-out request created successfully!", Severity.Success);
+
+                // Optional: refresh table or reload list
+                await LoadData();
+                StateHasChanged();
+            }
         }
         catch(Exception ex)
         {
             Logger.LogError(ex.Message, "Error in creating pull out request");
             SnackBar.Add("An error occured.", Severity.Error);
         }
+    }
+
+    protected void ViewPullOutDetails(int pullOutId)
+    {
+        NavigationManager.NavigateTo($"/view-pull-out-request/{pullOutId}");
     }
 }
