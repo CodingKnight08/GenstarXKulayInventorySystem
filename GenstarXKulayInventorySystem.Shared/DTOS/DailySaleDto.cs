2a55cb073d995493050822e8bf95f189f7c3f137
Jayson <macedajayson11@gmail.com>
2025-12-15 06:24:05 +0800

    update - DailySaleDto

--- GenstarXKulayInventorySystem.Shared/DTOS/DailySaleDto.cs
+++ GenstarXKulayInventorySystem.Shared/DTOS/DailySaleDto.cs
@@ -27,4 +27,5 @@ public class DailySaleDto:BaseEntityDto
     public bool IsApproved { get; set; }
     public bool IsChargedSales { get; set; }
     public List<SaleItemDto> SaleItems { get; set; } = new List<SaleItemDto>();
+    public List<ReturnItemDto> ReturnItems { get; set; } = new List<ReturnItemDto>();
 }
