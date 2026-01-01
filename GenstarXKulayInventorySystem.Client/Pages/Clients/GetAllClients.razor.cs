8636a3680f9eb02a25893ab2e6d2a4baa223af1c
maceda <macedajayson11@gmail.com>
2026-01-01 22:14:59 +0800

    update - Clients components

--- GenstarXKulayInventorySystem.Client/Pages/Clients/GetAllClients.razor.cs
+++ GenstarXKulayInventorySystem.Client/Pages/Clients/GetAllClients.razor.cs
@@ -8,20 +8,41 @@ namespace GenstarXKulayInventorySystem.Client.Pages.Clients;
 
 public partial class GetAllClients
 {
+    [Parameter, SupplyParameterFromQuery(Name = "pageskip")]
+    public int PageSkip { get; set; } = 0;
+    [Parameter, SupplyParameterFromQuery(Name = "pagetake")]
+    public int PageTake { get; set; } = 10;
     [Inject] protected HttpClient HttpClient { get; set; } = default!;
     [Inject] protected ILogger<GetAllClients> Logger { get; set; } = default!;
     [Inject] protected IDialogService DialogService { get; set; } = default!;
     [Inject] protected ISnackbar SnackBar { get; set; } = default!;
+    [Inject] protected UserState UserState { get; set; } = default!;
     [Inject] private NavigationManager NavigationManager { get; set; } = default!;
     protected List<ClientDto> Clients { get; set; } = new List<ClientDto>();
-    protected BranchOption SelectedBranch { get; set; } = BranchOption.GeneralSantosCity;
-
+    protected BranchOption SelectedBranch { get; set; }
+    protected int CurrentPageIndex { get; set; }
     protected bool IsLoading { get; set; } = false;
-    protected override async Task OnInitializedAsync()
+    protected override void OnInitialized()
     {
-        await LoadClients();
+        SelectedBranch = UserState.Branch.GetValueOrDefault();
     }
 
+    protected override async Task OnParametersSetAsync()
+    {
+        if (PageTake <= 0)
+            PageTake = 10;
+
+        if (PageSkip < 0)
+            PageSkip = 0;
+
+
+
+
+        CurrentPageIndex = PageTake > 0 ? PageSkip / PageTake : 0;
+
+        // Load sales whenever query parameters change
+        await LoadClients();
+    }
     protected async Task LoadClients()
     {
         IsLoading = true;
@@ -74,7 +95,7 @@ public partial class GetAllClients
 
     protected void ViewClientAsync(int id)
     {
-        NavigationManager.NavigateTo($"/clients/view/{id}");
+        NavigationManager.NavigateTo($"/clients/view/{id}?pageskip={PageSkip}&pagetake={PageTake}");
     }
 
     protected async Task EditClientAsync(int id)
@@ -124,5 +145,26 @@ public partial class GetAllClients
                 StateHasChanged();
             }
         }
+    }
+
+    private void OnPageChanged(int pageIndex)
+    {
+        CurrentPageIndex = pageIndex;
+        PageSkip = pageIndex * PageTake;
+        UpdateQuery();
+    }
+
+    private void OnRowsPerPageChanged(int size)
+    {
+        PageTake = size;
+        PageSkip = 0;
+        UpdateQuery();
+    }
+    private void UpdateQuery()
+    {
+        NavigationManager.NavigateTo(
+            $"/clients?pageskip={PageSkip}&pagetake={PageTake}",
+            replace: true
+        );
     }
 }
