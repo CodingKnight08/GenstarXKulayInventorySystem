997ea10a2197f97065f48bbd79f93fd5a6f96bd9
maceda <macedajayson11@gmail.com>
2026-01-21 14:18:00 +0800

    update - WayBill controller

--- GenstarXKulayInventorySystem.Server/Controllers/WayBillController.cs
+++ GenstarXKulayInventorySystem.Server/Controllers/WayBillController.cs
@@ -1,8 +1,60 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GenstarXKulayInventorySystem.Server.Services;
+using GenstarXKulayInventorySystem.Shared.DTOS;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GenstarXKulayInventorySystem.Server.Controllers;
 
-public class WayBillController : Controller
+[ApiController]
+[Route("api/[controller]")]
+public class WayBillController : ControllerBase
 {
-    
+    private readonly IWayBillService _waybillService;
+    private readonly ILogger<WayBillController> _logger;
+
+    public WayBillController(IWayBillService waybillService, ILogger<WayBillController> logger)
+    {
+        _waybillService = waybillService;
+        _logger = logger;
+    }
+
+    [HttpGet("all")]
+    public async Task<ActionResult<List<WayBillDto>>> GetAllWayBill()
+    {
+        var wayBills = await _waybillService.GetAllWayBills();
+        return Ok(wayBills);
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<WayBillDto>> GetWayBillById(int id)
+    {
+        var wayBill = await _waybillService.GetWayBillById(id);
+        if (wayBill == null)
+            return NotFound($"No waybill found");
+        return Ok(wayBill);
+    }
+
+    [HttpGet("items/{id:int}")]
+    public async Task<ActionResult<List<WayBillItemsDto>>> GetAllWayBillItems(int id)
+    { 
+        var wayBillItems = await _waybillService.GetAllWayBillItemsByWayBillId(id);
+        if (wayBillItems == null)
+            return NotFound();
+        return Ok(wayBillItems);
+    }
+
+    [HttpPost("create")]
+    public async Task<IActionResult> CreateWayBill(WayBillDto dto)
+    {
+        try
+        {
+            var result = await _waybillService.CreateWayBill(dto);
+            if (!result)
+                return BadRequest("Way bill already exist");
+            return Ok(result);
+        }
+        catch(Exception ex)
+        {
+            return StatusCode(500, ex.Message);
+        }
+    }
 }
