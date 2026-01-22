bf4c738549184dd1de144c05c32c5ba2f01d228e
maceda <macedajayson11@gmail.com>
2026-01-22 10:47:48 +0800

    create - WayBillDamageItem Dto update - WayBillDto and WayBillItemDto

---
+++ GenstarXKulayInventorySystem.Shared/DTOS/WayBillDamageItemDto.cs
@@ -0,0 +1,11 @@
+﻿namespace GenstarXKulayInventorySystem.Shared.DTOS;
+
+public class WayBillDamageItemDto : BaseEntityDto
+{
+    public int Id { get; set; }
+    public int? WayBillItemId { get; set; }
+    public WayBillItemsDto? WayBillItem { get; set; }
+    public decimal DamageQuantity { get; set; } = 0;
+    public decimal DamageAmount { get; set; } = 0;
+    public decimal TotalDamageCost { get; set; } = 0;
+}
