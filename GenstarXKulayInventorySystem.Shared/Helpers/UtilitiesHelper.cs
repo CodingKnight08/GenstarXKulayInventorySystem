3e323e0e20314dacaf9c8998222155c2e73b7f77
maceda <macedajayson11@gmail.com>
2026-01-24 15:10:53 +0800

    update - UtilitiesHelper

--- GenstarXKulayInventorySystem.Shared/Helpers/UtilitiesHelper.cs
+++ GenstarXKulayInventorySystem.Shared/Helpers/UtilitiesHelper.cs
@@ -192,7 +192,8 @@ public static class UtilitiesHelper
         Pending,
         OnTheWay,
         Delivered,
-        Cancelled
+        Cancelled,
+        Partial
     }
     public enum SaleSearchCategory
     {
