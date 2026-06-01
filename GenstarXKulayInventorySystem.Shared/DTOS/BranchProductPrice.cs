1d75f904e88960c47409ebd54e109fed5353dec1
maceda <macedajayson11@gmail.com>
2026-06-01 10:21:32 +0800

    update - BranchProductPrice

---
+++ GenstarXKulayInventorySystem.Shared/DTOS/BranchProductPrice.cs
@@ -0,0 +1,13 @@
+﻿namespace GenstarXKulayInventorySystem.Shared.DTOS;
+
+public class BranchProductPrice
+{
+    public int BranchProductId { get; set; }
+    public string BrandName { get; set; } = string.Empty;
+    public string ProductName { get; set; } = string.Empty;
+    public decimal? CostPrice { get; set; }
+
+    public decimal? RetailPrice { get; set; }
+
+    public decimal? WholeSalePrice { get; set; }
+}
