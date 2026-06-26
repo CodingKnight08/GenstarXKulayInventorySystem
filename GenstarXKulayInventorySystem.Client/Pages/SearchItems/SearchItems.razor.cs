30a86c5892d63e746ef21c7bd3ab86cff6905acf
maceda <macedajayson11@gmail.com>
2026-06-26 16:52:47 +0800

    update - SearchItems

--- GenstarXKulayInventorySystem.Client/Pages/SearchItems/SearchItems.razor.cs
+++ GenstarXKulayInventorySystem.Client/Pages/SearchItems/SearchItems.razor.cs
@@ -1,5 +1,75 @@
-﻿namespace GenstarXKulayInventorySystem.Client.Pages.SearchItems;
+﻿using GenstarXKulayInventorySystem.Shared.DTOS;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System.Net.Http.Json;
+using static GenstarXKulayInventorySystem.Shared.Helpers.ProductsEnumHelpers;
+
+namespace GenstarXKulayInventorySystem.Client.Pages.SearchItems;
 
 public partial class SearchItems
 {
+    [Inject] private HttpClient HttpClient { get; set; } = default!;
+    [Inject] private ILogger<SearchItems> Logger { get; set; } = default!;
+    [Inject] private UserState UserState { get; set; } = default!;
+    private List<BranchProductDto> AllProducts { get; set; } = new List<BranchProductDto>();
+    private IEnumerable<BranchProductDto> Results { get;set;} = new List<BranchProductDto>();
+    private BranchOption Branch { get; set; } 
+    private bool IsLoading { get; set; } = false;
+    private string SearchItem { get; set; } = string.Empty;
+    private bool IsBrand { get; set; }
+    protected override async Task OnInitializedAsync()
+    {
+        Branch = UserState.Branch.Value;
+        await LoadProducts();
+    }
+
+    private async Task LoadProducts()
+    {
+        IsLoading = true;
+
+        try
+        {
+            var url = $"api/product/all/stocks/{Branch}?isBrand={IsBrand}&searchText={Uri.EscapeDataString(SearchItem)}";
+
+            var response = await HttpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+
+            Results = await response.Content.ReadFromJsonAsync<List<BranchProductDto>>() ?? [];
+        }
+        catch (Exception ex)
+        {
+            Logger.LogError(ex, "An error occurred while loading products.");
+            Results = [];
+        }
+        finally
+        {
+            IsLoading = false;
+        }
+    }
+
+
+    private async Task Search()
+    {
+        
+
+        await LoadProducts();
+    }
+    private decimal GetWarehouseQuantity(BranchProductDto product)
+    {
+        if (product.MasterProductId is null)
+            return 0;
+
+        return AllProducts
+            .Where(x =>
+                x.Branch == BranchOption.Warehouse &&
+                x.MasterProductId == product.MasterProductId)
+            .Sum(x => x.ActualQuantity);
+    }
+    private async Task HandleKeyDown(KeyboardEventArgs e)
+    {
+        if (e.Key == "Enter")
+        {
+            await Search();
+        }
+    }
 }
