2f99fb51c8e4ffb4367e96bf664c48014ead86ee
maceda <macedajayson11@gmail.com>
2026-08-01 08:34:17 +0800

    update - TopSaleItemDto

---
+++ GenstarXKulayInventorySystem.Shared/DTOS/TopSaleItemDto.cs
@@ -0,0 +1,9 @@
+﻿namespace GenstarXKulayInventorySystem.Shared.DTOS;
+
+public class TopSaleItemDto
+{
+    public string BrandName { get; set; } = "";
+    public string ProductName { get; set; } = "";
+    public decimal QuantitySold { get; set; }
+    public decimal TotalSales { get; set; }
+}
